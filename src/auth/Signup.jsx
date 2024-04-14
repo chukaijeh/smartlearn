20697f1c959681cbc16c193c5eac13f97a6aba7d
Chukaijeh <chukaijeh@gmail.com>
2024-04-15 00:24:47 +0100

    added confirm password field and fix errors

--- src/auth/Signup.jsx
+++ src/auth/Signup.jsx
@@ -1,66 +1,76 @@
-import googleIcon from '../assets/images/icons8-google-48.png';
-import { Header } from './components/Header';
-import { Link } from 'react-router-dom';
-import { createAccount, googleSignup } from './services/auth';
-import { useState } from 'react';
-import "../assets/styles/App.css"
-import './styles/auth.css';
-import { GoogleLogin } from '@react-oauth/google';
-
+import googleIcon from "../assets/images/icons8-google-48.png";
+import { Header } from "./components/Header";
+import { Link } from "react-router-dom";
+import { createAccount, googleSignup } from "./services/auth";
+import { useState } from "react";
+import "../assets/styles/App.css";
+import "./styles/auth.css";
+import { GoogleLogin } from "@react-oauth/google";
 
 function SignUp() {
-  const [firstNameError, updateFirstNameError] = useState("")
-  const [lastNameError, updateLastNameError] = useState("")
-  const [emailError, updateEmailError] = useState("")
-  const [userAccountSuccessful, updateUserAccountSuccessful] = useState("")
-  const [passwordError, updatePasswordError] = useState("")
-  const [isSignUp, updateisSignUp] = useState(false)
+  const [firstNameError, updateFirstNameError] = useState("");
+  const [lastNameError, updateLastNameError] = useState("");
+  const [emailError, updateEmailError] = useState("");
+  const [userAccountSuccessful, updateUserAccountSuccessful] = useState("");
+  const [passwordError, updatePasswordError] = useState("");
+  const [isSignUp, updateisSignUp] = useState(false);
   async function validate(e) {
-    updateEmailError("")
-    updateFirstNameError("")
-    updateLastNameError("")
-    updatePasswordError("")
+    updateEmailError("");
+    updateFirstNameError("");
+    updateLastNameError("");
+    updatePasswordError("");
     const inputs = [...e.target];
     const values = inputs.map((i) => {
-      if (i.className === 'inputfield') {
-        return i.value
+      if (i.className === "inputfield") {
+        return i.value;
       }
-    })
-    const [firstname, lastname, email, passwd] = [...values]
+    });
+    const [firstname, lastname, email, gender, passwd, confirmPassword] = [
+      ...values,
+    ];
     let condition = true;
     if (!firstname) {
-      updateFirstNameError("Firstname cannot be empty")
+      updateFirstNameError("Firstname cannot be empty");
       condition = false;
-      updateisSignUp(false)
+      updateisSignUp(false);
     }
     if (!lastname) {
-      updateLastNameError("Lastname cannot be empty")
+      updateLastNameError("Lastname cannot be empty");
       condition = false;
-      updateisSignUp(false)
+      updateisSignUp(false);
     }
-    if (email) {
-      console.log("Email validated");
-    }
-    else {
-      updateEmailError("Email field required")
+    if (!email) {
+      updateEmailError("Email field required");
       condition = false;
-      updateisSignUp(false)
+      updateisSignUp(false);
     }
-    if (passwd && (passwd.length < 6)) {
-      updatePasswordError("Password should have a minimum of 6 characters")
+    if (passwd && confirmPassword) {
+      if (passwd.length < 6 || confirmPassword.length < 6) {
+        updatePasswordError("Password should have a minimum of 6 characters");
+        condition = false;
+        updateisSignUp(false);
+      }
+    } else {
+      updatePasswordError("Password field required");
       condition = false;
-      updateisSignUp(false)
-    }
-    if (!passwd) {
-      updatePasswordError("Password field required")
-      condition = false;
-      updateisSignUp(false)
+      updateisSignUp(false);
     }
     if (condition) {
-      let res = await createAccount({ firstname, lastname, email, passwd })
-      res.status ? updateUserAccountSuccessful(res.message) : updateEmailError(res.message)
-      updateisSignUp(false)
-      !res.status && updateUserAccountSuccessful('')
+      let res = await createAccount({
+        firstname,
+        lastname,
+        email,
+        gender,
+        passwd,
+        confirmPassword,
+      });
+      res.status
+        ? updateUserAccountSuccessful(res.message)
+        : res.message
+        ? updatePasswordError(res.message)
+        : updateEmailError(res.msg);
+      updateisSignUp(false);
+      !res.status && updateUserAccountSuccessful("");
     }
   }
   return (
@@ -69,31 +79,59 @@ function SignUp() {
         <Header />
       </Link>
       <div className="auth-main r-flex r-flex-column">
-        <div className='bg-image main-common'>
-          <div className='contain-text'>
-            <h1>You're only one sign-up <br /> away from greatness!</h1>
+        <div className="bg-image main-common">
+          <div className="contain-text">
+            <h1>
+              You're only one sign-up <br /> away from greatness!
+            </h1>
             <p>Your destiny is calling, signup and discover what's possible</p>
           </div>
         </div>
-        <div className='contain-form main-common'>
-          <form onSubmit={function (e) {
-            updateisSignUp(true)
-            e.preventDefault()
-            validate(e)
-          }}>
+        <div className="contain-form main-common">
+          <form
+            onSubmit={function (e) {
+              updateisSignUp(true);
+              e.preventDefault();
+              validate(e);
+            }}
+          >
             <h2>Sign up</h2>
             <p>Sign up and start your journey</p>
-            <input type="text" placeholder='First name' className='inputfield' />
+            <input
+              type="text"
+              placeholder="First name"
+              className="inputfield"
+            />
             <small>{firstNameError}</small>
-            <input type="text" placeholder='Last name' className='inputfield' />
+            <input type="text" placeholder="Last name" className="inputfield" />
             <small>{lastNameError}</small>
-            <input type="email" placeholder='Email' className='inputfield' />
+            <input type="email" placeholder="Email" className="inputfield" />
             <small>{emailError}</small>
-            <input type="password" placeholder='Pas*****' className='inputfield' />
+            <select name="" id="gender" className="inputfield">
+              <option value="male">male</option>
+              <option value="female">female</option>
+            </select>
+            <input
+              type="password"
+              placeholder="Password"
+              className="inputfield"
+            />
+            <input
+              type="password"
+              placeholder="Confirm password"
+              className="inputfield"
+            />
             <small>{passwordError}</small>
-            {
-              isSignUp ? <input type="submit" id="submit" value={"authenticating..."} disabled /> : <input type="submit" id="submit" value={"Sign Up"} />
-            }
+            {isSignUp ? (
+              <input
+                type="submit"
+                id="submit"
+                value={"authenticating..."}
+                disabled
+              />
+            ) : (
+              <input type="submit" id="submit" value={"Sign Up"} />
+            )}
             <div id="success">{userAccountSuccessful}</div>
             <div className="split">
               <div className="line"></div>
@@ -101,13 +139,18 @@ function SignUp() {
               <div className="line"></div>
             </div>
             <img src="https://icons8.com/icon/17949/google" alt="" />
-            <GoogleLogin onSuccess={(res) => {
-              googleSignup(res);
-            }} onError={(resError) => {
-              console.log(resError);
-            }} className="google"></GoogleLogin>
-            <p className='alt-login'>Already have an account ?
-              <Link to="/auth/login" > Login</Link></p>
+            <GoogleLogin
+              onSuccess={(res) => {
+                googleSignup(res);
+              }}
+              onError={(resError) => {
+                console.log(resError);
+              }}
+              className="google"
+            ></GoogleLogin>
+            <p className="alt-login">
+              Already have an account ?<Link to="/auth/login"> Login</Link>
+            </p>
           </form>
         </div>
       </div>
--- src/auth/services/auth.js
+++ src/auth/services/auth.js
@@ -1,52 +1,75 @@
 import { resolvePath } from "react-router-dom";
 
-export async function createAccount({firstname, lastname, email, passwd}){
-    console.log(firstname, lastname, email, passwd);
-    const response = await fetch("https://smart-learn-b3p2.onrender.com/api/auth/register/", {
-        method: "post",
-        headers: {
-            'Content-Type': "application/json",
-        },
-        body: JSON.stringify({firstname, lastname, email, password: passwd})
-    })
-    try {
-      const res = await response.json()
-      return res
-    } catch (error) {
-        console.error(error)
+export async function createAccount({
+  firstname,
+  lastname,
+  email,
+  gender,
+  passwd,
+  confirmPassword,
+}) {
+  const response = await fetch(
+    "https://smart-learn-b3p2.onrender.com/api/auth/register/",
+    {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        firstname,
+        lastname,
+        email,
+        gender,
+        password: passwd,
+        confirmPassword,
+      }),
     }
-    return response
+  );
+  try {
+    const res = await response.json();
+    return res;
+  } catch (error) {
+    console.error(error);
+  }
+  return response;
 }
-export function googleSignup(cred){
-    const req = fetch("https://www.googleapis.com/oauth2/v1/userinfo", {
-        headers:{
-            Authorization: `Bearer ${cred.credential}`
-        }
+export function googleSignup(cred) {
+  const req = fetch("https://www.googleapis.com/oauth2/v1/userinfo", {
+    headers: {
+      Authorization: `Bearer ${cred.credential}`,
+    },
+  })
+    .then((res) => {
+      return res.json();
     })
-    .then(res=>{
-        return res.json()
-    }).then(data=>{
-        console.log(data);
-    }).catch(err=>{
-        console.log(err);
+    .then((data) => {
+      console.log(data);
     })
+    .catch((err) => {
+      console.log(err);
+    });
 }
 
-export function loginAuth({email, password}) {
-    const response = fetch("https://smart-learn-b3p2.onrender.com/api/auth/login/", {
-        method: "post",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({email, password})
+export function loginAuth({ email, password }) {
+  const response = fetch(
+    "https://smart-learn-b3p2.onrender.com/api/auth/login/",
+    {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    }
+  )
+    .then((res) => {
+      return res.json();
     })
-    .then(res=>{
-        return res.json()
-    }).then(body=>{
-        // console.log(body);
-        return body;
-    }).catch(err=>{
-        console.log(err);
+    .then((body) => {
+      // console.log(body);
+      return body;
     })
-    return response
+    .catch((err) => {
+      console.log(err);
+    });
+  return response;
 }
